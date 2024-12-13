b20cfce34500c5511b2c4ce2e7e205d0687c0e09
angeldoromedez <angeldmedez@gmail.com>
2024-12-13 22:38:38 +0800

    updated Register, Login and Logout Functions

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
 
+// Auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
-
-use App\Http\Controllers\PostController;
-
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/user', [AuthController::class, 'update']);
+    Route::delete('/user', [AuthController::class, 'delete']);
     Route::apiResource('posts', PostController::class);
 });
